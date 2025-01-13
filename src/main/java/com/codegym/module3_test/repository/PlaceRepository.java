37dc5b26dfa32dd4e0ab46cd82fd6f18bdcd5328
hoangNH <hoangcdnbk@gmail.com>
2025-01-13 21:54:21 +0700

    add code

--- src/main/java/com/codegym/module3_test/repository/PlaceRepository.java
+++ src/main/java/com/codegym/module3_test/repository/PlaceRepository.java
@@ -47,7 +47,7 @@ public class PlaceRepository {
             }
             query += " where end_date <= " + endDate;
         }
-
+        query += " order by  acreage desc";
         try (
             Connection connection = BaseRepository.getConnection();
             PreparedStatement statement = connection.prepareStatement(query)
